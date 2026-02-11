f7abd7fac917a15a8e1a5e99e55cb8e021036442
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-02-11 11:19:48 +0530

    Updated

--- PY_Lab/exp_3.py
+++ PY_Lab/exp_3.py
@@ -1,9 +1,25 @@
+# <----------------------------------- PART A ----------------------------------->
+
 # n = int(input("Enter a number: "))
 # if n % 2 == 0:
 #     print("Even number")
 # else:
 #     print("Odd number")
 
-# for i in range(2, 11):
-#     print(1/i)
+# <----------------------------------- PART B ----------------------------------->
+
+for i in range(2, 11):
+    print(1/i)
+
+# <----------------------------------- Practice ----------------------------------->
+
+# n = int(input("Enter a number: "))
+# if n % 5 == 0 and n % 11 == 0:
+#     print("number is divisible by 5 and 11")
+# elif n%5 == 0:
+#     print("5 se divisible")
+# elif n%11 == 0:
+#     print("11 se divisible")
+# else:
+#     print("dono se divisible nahi")
 
