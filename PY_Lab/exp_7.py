2a6ff517c8d037122155598592e8c6fa7a1b1d91
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-03-24 14:41:27 +0530

    Updated

--- PY_Lab/exp_7.py
+++ PY_Lab/exp_7.py
@@ -1,13 +1,16 @@
-with open("C:\\Users\\madha\\OneDrive\\Desktop\\py_lab\\PY_Lab\\madhav.txt", "r") as file:    
-    text = file.read()
+# ------------------- PART - 1 -------------------
+# with open("C:\\Users\\madha\\OneDrive\\Desktop\\py_lab\\PY_Lab\\madhav.txt", "r") as file:    
+#     text = file.read()
 
-freq = {}
+# freq = {}
 
-for ch in text:
-    if ch in freq:
-        freq[ch] += 1
-    else:
-        freq[ch] = 1
+# for ch in text:
+#     if ch in freq:
+#         freq[ch] += 1
+#     else:
+#         freq[ch] = 1
 
-for key, value in freq.items():
-    print(f"'{key}' : {value}") 
+# for key, value in freq.items():
+#     print(f"'{key}' : {value}") 
+
+# ------------------- PART - 2 -------------------
