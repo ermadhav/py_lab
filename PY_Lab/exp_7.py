1c773c6c2a2dbf614122c044677efd00d3730a2f
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-03-24 14:37:31 +0530

    Updated

--- PY_Lab/exp_7.py
+++ PY_Lab/exp_7.py
@@ -1,17 +1,13 @@
-# Open file in read mode
 with open("madhav.txt", "r") as file:
     text = file.read()
 
-# Dictionary to store frequency
 freq = {}
 
-# Count frequency of each character
 for ch in text:
     if ch in freq:
         freq[ch] += 1
     else:
         freq[ch] = 1
 
-# Print frequency
 for key, value in freq.items():
     print(f"'{key}' : {value}")
